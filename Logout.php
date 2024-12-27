7ebe95d41a8d8be1e6cac4c2f0b6c6f8378e245b
Edgard Fichtenau <edf4625@thi.de>
2024-12-27 14:38:56 +0100

    css datei geleert und bootstrap ueberall eingebunden

--- Logout.php
+++ Logout.php
@@ -12,7 +12,8 @@ session_unset();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logout</title>
-    <link rel="stylesheet"  href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
     <div class="content">
@@ -21,6 +22,9 @@ session_unset();
     <p class="pcenter">See u!</p>
     <a class="center" href="Login.php" class="center">Login again</a>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
     
 </body>
 </html>
